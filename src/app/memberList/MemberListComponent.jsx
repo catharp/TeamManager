daeca75c87cd542ef999688842bc86aa0228220b
Collin Tharp <catharp@gmail.com>
2017-03-12 15:20:39 -0700

    [MOD] Display member details in MemberList

--- src/app/memberList/MemberListComponent.jsx
+++ src/app/memberList/MemberListComponent.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default ({ teamMembers }) => {
   return (
     <div className="container member-list">
-      { teamMembers.map(member => (<div key={ member.id }>{ member.name }</div>))}
+      <h2>Team members</h2>
+      <p>You have { teamMembers.length } team members.</p>
+      { teamMembers.map(member => (
+        <div className="member-details" key={ member.id }>
+          <h4>{ member.name } { member.role === 'Admin' ? '(admin)' : null }</h4>
+          <p>{ member.phone }</p>
+          <p>{ member.email }</p>
+        </div>)
+      )}
     </div>
   )
 }
