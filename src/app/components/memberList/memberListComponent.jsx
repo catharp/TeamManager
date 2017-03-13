102a1ccd70513d6f2640a1d16d5ae0511654f53a
Collin Tharp <catharp@gmail.com>
2017-03-12 21:41:55 -0700

    [MOD] Split team members names into first and last

--- src/app/components/memberList/memberListComponent.jsx
+++ src/app/components/memberList/memberListComponent.jsx
@@ -5,13 +5,13 @@ export default ({ teamMembers }) => (
   <div className="container member-list">
     <span className="btn-corner glyphicon glyphicon-plus" onClick={ () => browserHistory.push('/addMember') } />
     <h1>Team members</h1>
-    <h3>You have { teamMembers.length } team members.</h3>
+    <h3 className="subtitle">You have { teamMembers.length } team members.</h3>
     <div className="divider"></div>
     { Object.keys(teamMembers).map( (memberId, i, arr, member = teamMembers[memberId]) => (
       <div className="list-item" key={ memberId }>
         <img className="member-photo" src={ member.photo || "../../static/assets/defaultUser.png"} />
         <div className="member-info">
-          <h4>{ member.name } { member.role === 'Admin' ? '(admin)' : null }</h4>
+          <h4>{ member.firstName } { member.lastName } { member.role === 'Admin' ? '(admin)' : null }</h4>
           <p>{ member.phone }</p>
           <p>{ member.email }</p>
         </div>
--- src/app/reducers/teamReducer.js
+++ src/app/reducers/teamReducer.js
@@ -1,28 +1,32 @@
 const defaultTeam = {
   1: {
     photo: 'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-    name: 'David Banner',
+    firstName: 'David',
+    lastName: 'Banner',
     email: 'bruce@culver.gov',
     phone: '800-HULK-OUT',
     role: 'Admin'
   },
   2: {
     photo: 'https://i.annihil.us/u/prod/marvel/i/mg/9/30/538cd33e15ab7/standard_xlarge.jpg',
-    name: 'Peter Parker',
+    firstName: 'Peter',
+    lastName: 'Parker',
     email: 'pete@horizon.io',
     phone: '800-WEB-SRVR',
     role: 'Regular'
   },
   3: {
     photo: 'https://i.annihil.us/u/prod/marvel/i/mg/6/a0/55b6a25e654e6/standard_xlarge.jpg',
-    name: 'Tony Stark',
+    firstName: 'Tony',
+    lastName: 'Stark',
     email: 'tony@stark.com',
     phone: '800-IRON-MAN',
     role: 'Regular'
   },
   4: {
     photo: 'https://i.annihil.us/u/prod/marvel/i/mg/3/50/537ba56d31087/standard_xlarge.jpg',
-    name: 'Steve Rogers',
+    firstName: 'Steve',
+    lastName: 'Rogers',
     email: 'srogers@usa.gov',
     phone: '800-AMERICA',
     role: 'Regular'
