40ad9935a44eb26e3245275b9e8d309ec4ae2e45
Collin Tharp <catharp@gmail.com>
2017-03-13 13:26:33 -0700

    [MOD] Refactor components to use Links from React Router

--- src/app/components/addMember/addMemberComponent.jsx
+++ src/app/components/addMember/addMemberComponent.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 
 import MemberInfoForm from '../../forms/memberInfoForm';
 
 export default ({ addMember }) => (
   <div className="container add-member">
-    <span className="btn-corner glyphicon glyphicon-remove" onClick={ () => browserHistory.push('/') } />
+    <Link to="/"><span className="btn-corner glyphicon glyphicon-remove" /></Link>
     <h1>Add a team member</h1>
     <h3 className="subtitle">Set email, location and role.</h3>
     <div className="divider"></div>
--- src/app/components/memberList/memberListComponent.jsx
+++ src/app/components/memberList/memberListComponent.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import { Link } from 'react-router';
 
 export default ({ team, setEditting }) => (
   <div className="container member-list">
-    <span className="btn-corner glyphicon glyphicon-plus" onClick={ () => browserHistory.push('/addMember') } />
+    <Link to="/addMember"><span className="btn-corner glyphicon glyphicon-plus" /></Link>
     <h1>Team members</h1>
     <h3 className="subtitle">You have { team.members.length } team members.</h3>
     <div className="divider"></div>
     { team.members.map( (member, index) => (
-      <div className="list-item" key={ member.firstName + member.lastName } onClick= { () => { setEditting(index); browserHistory.push('/editMember') } }>
-        <img className="member-photo" src={ member.photo || "../../static/assets/defaultUser.png"} />
-        <div className="member-info">
-          <h4>{ member.firstName } { member.lastName } { member.role === 'Admin' ? '(admin)' : null }</h4>
-          <p>{ member.phone }</p>
-          <p>{ member.email }</p>
+      <Link key={ member.firstName + member.lastName } onClick={ () => setEditting(index) } to="/editMember">
+        <div className="list-item">
+          <img className="member-photo" src={ member.photo || "../../static/assets/defaultUser.png"} />
+          <div className="member-info">
+            <h4>{ member.firstName } { member.lastName } { member.role === 'Admin' ? '(admin)' : null }</h4>
+            <p>{ member.phone }</p>
+            <p>{ member.email }</p>
+          </div>
         </div>
-      </div>
+      </Link>
     ))}
   </div>
 )
