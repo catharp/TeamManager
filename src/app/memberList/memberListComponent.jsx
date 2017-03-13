56584bfeb5526faeb7e1db12d809588fdbc9fc52
Collin Tharp <catharp@gmail.com>
2017-03-12 18:24:02 -0700

    [MOD] Add AddMember button to MembersList

--- src/app/memberList/memberListComponent.jsx
+++ src/app/memberList/memberListComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 
 export default ({ teamMembers }) => {
   return (
     <div className="container member-list">
+      <span className="add-member glyphicon glyphicon-plus" onClick={ () => browserHistory.push('/addMember') } />
       <h1>Team members</h1>
       <h3>You have { teamMembers.length } team members.</h3>
       { teamMembers.map( member => (
         <div className="member-details" key={ member.id }>
-          <img className="member-photo" src={ member.photo || "../static/assets/defaultUser.png"}></img>
+          <img className="member-photo" src={ member.photo || "../static/assets/defaultUser.png"} />
           <div className="member-info">
             <h4>{ member.name } { member.role === 'Admin' ? '(admin)' : null }</h4>
             <p>{ member.phone }</p>
