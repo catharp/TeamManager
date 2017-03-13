6059a1e75b955d89fb3cf57f552471c4d130b034
Collin Tharp <catharp@gmail.com>
2017-03-12 21:28:13 -0700

    [MOD] Refactor list item styles to be global

--- src/app/components/memberList/memberListComponent.jsx
+++ src/app/components/memberList/memberListComponent.jsx
@@ -6,15 +6,16 @@ export default ({ teamMembers }) => (
     <span className="btn-corner glyphicon glyphicon-plus" onClick={ () => browserHistory.push('/addMember') } />
     <h1>Team members</h1>
     <h3>You have { teamMembers.length } team members.</h3>
+    <div className="divider"></div>
     { Object.keys(teamMembers).map( (memberId, i, arr, member = teamMembers[memberId]) => (
-      <div className="member-details" key={ memberId }>
+      <div className="list-item" key={ memberId }>
         <img className="member-photo" src={ member.photo || "../../static/assets/defaultUser.png"} />
         <div className="member-info">
           <h4>{ member.name } { member.role === 'Admin' ? '(admin)' : null }</h4>
           <p>{ member.phone }</p>
           <p>{ member.email }</p>
         </div>
-      </div>)
-    )}
+      </div>
+    ))}
   </div>
 )
