792e8fc91be45eecb3fee3eaaa768ec5963a05db
Collin Tharp <catharp@gmail.com>
2017-03-12 22:13:22 -0700

    [MOD] Add redux-form to dependencies and reducers

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import teamReducer from './teamReducer';
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   teamMembers: teamReducer,
+  form: formReducer
 });
 
 export default rootReducer;
