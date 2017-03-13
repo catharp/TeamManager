51a91a9a64866b87af9eb839fbc77fbbee5b2784
Collin Tharp <catharp@gmail.com>
2017-03-13 12:31:42 -0700

    [MOD] Extract AddMember form into reusable MemberInfoForm

--- src/app/components/addMember/addMemberComponent.jsx
+++ src/app/components/addMember/addMemberComponent.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
-import { Field, reduxForm } from 'redux-form';
 
-const AddMember = ({ handleSubmit }) => (
+import MemberInfoForm from '../../forms/memberInfoForm';
+
+export default () => (
   <div className="container add-member">
     <span className="btn-corner glyphicon glyphicon-remove" onClick={ () => browserHistory.push('/') } />
     <h1>Add a team member</h1>
     <h3 className="subtitle">Set email, location and role.</h3>
     <div className="divider"></div>
-    <h3>Info</h3>
-    <form onSubmit={ handleSubmit }>
-      <Field name="firstName" component="input" type="text" />
-      <Field name="lastName" component="input" type="text" />
-      <Field name="email" component="input" type="text" />
-      <Field name="phone" component="input" type="text" />
-      <h3>Role</h3>
-      <p className="list-item"><Field name="role" component="input" type="radio" value="regular" />Regular - Can't delete members</p>
-      <p className="list-item"><Field name="role" component="input" type="radio" value="admin" />Admin - Can delete members</p>
-      <button type="submit">Save</button>
-    </form>
+    <MemberInfoForm onSubmit={ memberInfo => console.log(memberInfo) } />
   </div>
 )
-
-export default reduxForm({
-  form: 'addMember',
-  initialValues: {role: 'regular'}
-})(AddMember);
---
+++ src/app/forms/memberInfoForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+const MemberInfoForm = ({ handleSubmit }) => (
+  <form onSubmit={ handleSubmit }>
+    <h3>Info</h3>
+    <Field name="firstName" component="input" type="text" />
+    <Field name="lastName" component="input" type="text" />
+    <Field name="email" component="input" type="text" />
+    <Field name="phone" component="input" type="text" />
+    <h3>Role</h3>
+    <p className="list-item"><Field name="role" component="input" type="radio" value="regular" />Regular - Can't delete members</p>
+    <p className="list-item"><Field name="role" component="input" type="radio" value="admin" />Admin - Can delete members</p>
+    <button type="submit">Save</button>
+  </form>
+)
+
+export default reduxForm({
+  form: 'addMember',
+  initialValues: {role: 'regular'}
+})(MemberInfoForm);
